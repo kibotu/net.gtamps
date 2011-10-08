ebc40f4665757bca8195bea2502159d95f53f334
Jan Rabe <cloudgazer3d@gmail.com>
2011-10-08 23:28:39 +0200

    fixed a nullpointer bug

--- android/src/net/gtamps/android/game/client/RemoteInputDispatcher.java
+++ android/src/net/gtamps/android/game/client/RemoteInputDispatcher.java
@@ -3,6 +3,7 @@ package net.gtamps.android.game.client;
 import net.gtamps.android.core.utils.Utils;
 import net.gtamps.shared.Config;
 import net.gtamps.shared.communication.Message;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.DataInputStream;
@@ -16,10 +17,10 @@ public class RemoteInputDispatcher implements Runnable {
 
     private volatile boolean isRunning;
     @Nullable
-    private DataInputStream inputStream;
-    private ConcurrentLinkedQueue<Message> inbox;
+    private final DataInputStream inputStream;
+    private final ConcurrentLinkedQueue<Message> inbox;
 
-    public RemoteInputDispatcher(DataInputStream inputStream, ConcurrentLinkedQueue<Message> inbox) {
+    public RemoteInputDispatcher(@Nullable DataInputStream inputStream, @NotNull ConcurrentLinkedQueue<Message> inbox) {
         isRunning = false;
         this.inputStream = inputStream;
         this.inbox = inbox;
@@ -40,33 +41,29 @@ public class RemoteInputDispatcher implements Runnable {
         Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
 
         byte [] response;
-        int length = 0;
+        int length;
 
         while(isRunning) {
             try {
 
                 //  latency
                 try {
-                    Thread.currentThread().sleep(Config.SOCKET_LATENCY);
+                    Thread.sleep(Config.SOCKET_LATENCY);
                 } catch (InterruptedException e) {
                      Utils.log(TAG, e.getMessage());
                 }
 
                 // build message
-                length = ((int)((byte) inputStream.read()) << 8) + ((int)((byte) inputStream.read() & 0xFF));
+                length = ((int)((byte) inputStream.read()) << 8) + inputStream.read();
                 response = new byte[length];
                 Utils.log(TAG, "has received "+ length + " bytes");
                 inputStream.readFully(response);
                 Message message = MessageFactory.deserialize(response);
                 if(message != null) inbox.add(message);
 
-                /** var 2 **/
-//                response = inputStream.readUTF().getBytes();
-//                if(response != null && response.length > 1) {
-//                    Utils.log(TAG, "has received "+length + " bytes");
-//                    Message message = MessageFactory.deserialize(response);
-//                }
             } catch (IOException e) {
+                Utils.log(TAG, e.getMessage());
+            } catch (NullPointerException e) {
                 Utils.log(TAG, e.getMessage());
             }
         }
