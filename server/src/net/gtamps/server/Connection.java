c3c568c4104c6c100ae8f82195456b5ea0307751
til <til.boerner@gmx.net>
2011-10-11 15:42:30 +0200

    Connection

--- server/src/net/gtamps/server/Connection.java
+++ server/src/net/gtamps/server/Connection.java
@@ -1,13 +1,16 @@
 package net.gtamps.server;
 
+import org.xsocket.connection.INonBlockingConnection;
+
 import net.gtamps.shared.communication.ISerializer;
 import net.gtamps.shared.communication.Message;
 
 public class Connection {
 	private final ISocketHandler socketHandler;
 	private final ISerializer serializer;
+	private final INonBlockingConnection nbc;
 
-	public Connection(ISocketHandler socketHandler, ISerializer serializer) {
+	public Connection(INonBlockingConnection nbc, ISocketHandler socketHandler, ISerializer serializer) {
 		if (socketHandler == null) {
 			throw new IllegalArgumentException("'socketHandler' must not be null");
 		}
@@ -16,11 +19,11 @@ public class Connection {
 		}
 		this.socketHandler = socketHandler;
 		this.serializer = serializer;
+		this.nbc = nbc;
 	}
 	
 	public void send(Message msg) {
-		// TODO Auto-generated method stub
-		
+		socketHandler.send(nbc, serializer.serializeMessage(msg));
 	}
 	
 
