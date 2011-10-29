5f216d37012430d6e9ed2fdfaeb448d706d8d5b2
til <til.boerner@gmx.net>
2011-10-29 08:05:19 +0200

    reworked session handling: sessions are now immutable and don't get passed around

--- server/src/net/gtamps/server/User.java
+++ server/src/net/gtamps/server/User.java
@@ -7,20 +7,50 @@ public final class User {
 	
 	public final int uid;
 	public final String name;
+	public transient final String password;
 
-	public User(int id, String name) {
+	public User(final int id, final String name) {
 		if (name == null) {
 			throw new IllegalArgumentException("'name' must not be null");
 		}
 		if (!isValidId(id)) {
 			throw new IllegalArgumentException("'id' is invalid");
 		}
-		this.uid = id;
+		uid = id;
 		this.name = name;
+		password = null;
 	}
 	
-	private boolean isValidId(int id) {
+	private boolean isValidId(final int id) {
 		return id > 0;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + uid;
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof User)) {
+			return false;
+		}
+		final User other = (User) obj;
+		if (uid != other.uid) {
+			return false;
+		}
+		return true;
+	}
+	
+	
 	
 }
