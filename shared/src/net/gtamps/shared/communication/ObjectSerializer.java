7d2e1f5ad4a05078f837a3b04d89c0a71a155ef8
til <til.boerner@gmx.net>
2011-11-03 19:19:19 +0100

    debugging

--- shared/src/net/gtamps/shared/communication/ObjectSerializer.java
+++ shared/src/net/gtamps/shared/communication/ObjectSerializer.java
@@ -1,9 +1,19 @@
 package net.gtamps.shared.communication;
 
-import net.gtamps.shared.Utils.Logger;
-import org.jetbrains.annotations.NotNull;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
-import java.io.*;
+import net.gtamps.shared.Utils.Logger;
+import net.gtamps.shared.communication.data.UpdateData;
+import net.gtamps.shared.game.GameObject;
+import net.gtamps.shared.game.IProperty;
+import net.gtamps.shared.game.entity.Entity;
+import net.gtamps.shared.game.event.GameEvent;
+
+import org.jetbrains.annotations.NotNull;
 
 public class ObjectSerializer implements ISerializer{
 
@@ -19,6 +29,23 @@ public class ObjectSerializer implements ISerializer{
 
     @Override
     public byte[] serializeMessage(@NotNull Message message) {
+    	
+    	//DEBUG
+    	for (Sendable sendable : message.sendables) {
+    		if (sendable.type.equals(SendableType.GETUPDATE_OK)) {
+    			UpdateData data = (UpdateData) sendable.data;
+    			for (GameObject go : data.gameObjects) {
+    				Class<? extends GameObject> c = go.getClass();
+    				if (!Entity.class.isAssignableFrom(c) || !GameEvent.class.isAssignableFrom(c)) {
+    					throw new IllegalStateException(go.toString());
+    				}
+    				for (IProperty p : go.getAllProperties()) {
+    					System.out.println(p.toString());
+    				}
+    			}
+    		}
+    	}
+    	
         try {
             byteOutputStream = new ByteArrayOutputStream();
             objectOutputStream = new ObjectOutputStream(byteOutputStream);
