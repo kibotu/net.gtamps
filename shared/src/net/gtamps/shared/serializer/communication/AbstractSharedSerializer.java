4856208240be1dd6c2cd84605e7a7a9d323726fa
til <til.boerner@gmx.net>
2011-11-04 23:58:37 +0100

    deprecated AbstractSharedSerializer in preparation for integration of SharedObject

--- shared/src/net/gtamps/shared/serializer/communication/AbstractSharedSerializer.java
+++ shared/src/net/gtamps/shared/serializer/communication/AbstractSharedSerializer.java
@@ -1,9 +1,7 @@
 package net.gtamps.shared.serializer.communication;
 
-import java.lang.reflect.Field;
-import java.util.HashSet;
-import java.util.Set;
-
+//TODO not needed anymore, as soon as ISerializer takes SharedObjects as arguments
+@Deprecated
 public abstract class AbstractSharedSerializer implements ISerializer {
 	
 	private static final String SHAREABLE_FIELD_MARKER = "_4e4b219008d44271";
@@ -25,22 +23,6 @@ public abstract class AbstractSharedSerializer implements ISerializer {
 	}
 	
 	private boolean isShareable(Object o) {
-		String fullName = c.getCanonicalName(); 
-		if (!fullName.startsWith("java.")) {
-			try {
-				c.getField(SHAREABLE_FIELD_MARKER);
-			} catch (SecurityException e) {
-				e.printStackTrace();
-			} catch (NoSuchFieldException e) {
-				return false;
-			}
-		}
-		Field[] ownFields = c.getDeclaredFields();
-		for (Field ownField: ownFields) {
-			if (!isShareable(ownField.)) {
-				return false;
-			}
-		}
 		return true;
 	}
 
