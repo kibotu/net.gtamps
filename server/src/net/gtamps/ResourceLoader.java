ead0c75bf5947dd4a7f3fa1d22304559d08b4fe2
til <til.boerner@gmx.net>
2011-11-10 21:26:56 +0100

    started implementation of level loader

--- server/src/net/gtamps/ResourceLoader.java
+++ server/src/net/gtamps/ResourceLoader.java
@@ -1,37 +1,50 @@
 package net.gtamps;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
 
-import java.io.*;
-import java.util.Scanner;
-
 public class ResourceLoader {
-
-    private static final SAXBuilder saxBuilder = new SAXBuilder();
-
-    public static Element getFileAsXml(String path) throws JDOMException, IOException {
-        Element e = null;
-        Document doc = saxBuilder.build(new File(path));
-        e = doc.getRootElement();
-        return e;
-    }
-
-    public static String getFileAsString(String path) throws FileNotFoundException {
-        Scanner sc = null;
-        String fileContent = "";
-        try {
-            sc = new Scanner(new BufferedReader(new FileReader(path)));
-            fileContent = "";
-            while (sc.hasNextLine()) {
-                fileContent += sc.nextLine();
-            }
-        } finally {
-            sc.close();
-        }
-        return fileContent;
-    }
+	
+	private static final SAXBuilder saxBuilder = new SAXBuilder();
+	
+	public static Element getFileAsXml(final String path) throws JDOMException, IOException {
+		Element e = null;
+		final Document doc = saxBuilder.build(new File(path));
+		e = doc.getRootElement();
+		return e;
+	}
+	
+	public static InputStream getFileAsInputStream(final String path) throws FileNotFoundException {
+		final File f = new File(path);
+		final InputStream in = new BufferedInputStream(new FileInputStream(f));
+		return in;
+	}
+	
+	public static String getFileAsString(final String path) throws FileNotFoundException {
+		Scanner sc = null;
+		String fileContent = "";
+		try {
+			sc = new Scanner(new BufferedReader(new FileReader(path)));
+			fileContent = "";
+			while(sc.hasNextLine()) {
+				fileContent += sc.nextLine();
+			}
+		} finally {
+			sc.close();
+		}
+		return fileContent;
+	}
 
 }
