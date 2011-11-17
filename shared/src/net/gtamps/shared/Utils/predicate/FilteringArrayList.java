42386ae5ae2605c8afb577b4a38837a9008f873a
til <til.boerner@gmx.net>
2011-11-17 18:08:08 +0100

    minor work on shared.Utils.predicates: 	modified:   shared/src/net/gtamps/shared/Utils/predicate/FilteringArrayList.java 	new file:   shared/src/net/gtamps/shared/Utils/predicate/Predicates.java

--- shared/src/net/gtamps/shared/Utils/predicate/FilteringArrayList.java
+++ shared/src/net/gtamps/shared/Utils/predicate/FilteringArrayList.java
@@ -5,46 +5,57 @@ import java.util.Arrays;
 
 
 public class FilteringArrayList<T> extends ArrayList<T> implements FilteringCollection<T> {
-    public static <A> FilteringArrayList<A> fromArray(final A[] a) {
-        final FilteringArrayList<A> list = new FilteringArrayList<A>();
-        list.ensureCapacity(a.length);
-        list.addAll(Arrays.asList(a));
-        return list;
-    }
 
-    @Override
-    public FilteringArrayList<T> retainAll(final Predicate<T> p) {
-        removeAll(PredicateModifier.not(p));
-        return this;
-    }
+	private static final long serialVersionUID = -5615638962150029180L;
 
-    @Override
-    public FilteringArrayList<T> removeAll(final Predicate<T> p) {
-        int i = 0;
-        while (i < size()) {
-            if (p.isTrueFor(get(i))) {
-                remove(i);
-            } else {
-                i++;
-            }
-        }
-        return this;
-    }
+	public static <A> FilteringArrayList<A> fromArray(final A[] a) {
+		final FilteringArrayList<A> list = new FilteringArrayList<A>();
+		list.ensureCapacity(a.length);
+		list.addAll(Arrays.asList(a));
+		return list;
+	}
 
-    @Override
-    public boolean trueForAll(final Predicate<T> p) {
-        return !trueForOne(PredicateModifier.not(p));
-    }
+	public FilteringArrayList() {
+		super();
+	}
 
-    @Override
-    public boolean trueForOne(final Predicate<T> p) {
-        final int size = size();
-        for (int i = 0; i < size; i++) {
-            if (p.isTrueFor(get(i))) {
-                return true;
-            }
-        }
-        return false;
-    }
+	public FilteringArrayList(final int initialCapacity) {
+		super(initialCapacity);
+	}
+
+	@Override
+	public FilteringArrayList<T> retainAll(final Predicate<T> p) {
+		removeAll(PredicateModifier.not(p));
+		return this;
+	}
+
+	@Override
+	public FilteringArrayList<T> removeAll(final Predicate<T> p) {
+		int i = 0;
+		while (i < size()) {
+			if (p.isTrueFor(get(i))) {
+				remove(i);
+			} else {
+				i++;
+			}
+		}
+		return this;
+	}
+
+	@Override
+	public boolean trueForAll(final Predicate<T> p) {
+		return !trueForOne(PredicateModifier.not(p));
+	}
+
+	@Override
+	public boolean trueForOne(final Predicate<T> p) {
+		final int size = size();
+		for (int i = 0; i < size; i++) {
+			if (p.isTrueFor(get(i))) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
