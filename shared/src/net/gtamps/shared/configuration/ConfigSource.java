250ffd8acfb3081cb144f718d5cc687ebddba4b2
til <til.boerner@gmx.net>
2011-12-07 22:41:38 +0100

    shared..ConfigSource(File) now tries to use canonical path

--- shared/src/net/gtamps/shared/configuration/ConfigSource.java
+++ shared/src/net/gtamps/shared/configuration/ConfigSource.java
@@ -1,6 +1,7 @@
 package net.gtamps.shared.configuration;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
 public final class ConfigSource extends AbstractConfigSource {
@@ -19,17 +20,14 @@ public final class ConfigSource extends AbstractConfigSource {
 	 * @param url	not {@code null}
 	 */
 	public ConfigSource(final URL url) {
-		super(url.getRef() != null ? 
-				url.toString().substring(url.toString().lastIndexOf(url.getRef())) :
-					url.toString(),
-					URL.class);
+		super(getURLStringWithoutRef(url), URL.class);
 	}
 
 	/**
 	 * @param file	not {@code null}
 	 */
 	public ConfigSource(final File file) {
-		super(file.getAbsolutePath(), File.class);
+		super(getFilePath(file), File.class);
 	}
 
 	/**
@@ -60,5 +58,19 @@ public final class ConfigSource extends AbstractConfigSource {
 		return new ConfigSource(this);
 	}
 
+	private static String getFilePath(final File file)  {
+		try {
+			return file.getCanonicalPath();
+		} catch (final IOException e) {
+			e.printStackTrace();
+			return file.getAbsolutePath(); 
+		}
+	}
+
+	private static String getURLStringWithoutRef(final URL url) {
+		return url.getRef() != null ? 
+				url.toString().substring(url.toString().lastIndexOf(url.getRef())) :
+					url.toString();
+	}
 
 }
