2632b6fd28bb84b8f7dbb0b8605cb940ecec7cff
til <til.boerner@gmx.net>
2011-12-21 19:21:10 +0100

    server: universe is general eventRoot

--- server/src/net/gtamps/game/physics/Box2DEngine.java
+++ server/src/net/gtamps/game/physics/Box2DEngine.java
@@ -9,51 +9,51 @@ import org.jbox2d.dynamics.Body;
 import org.jbox2d.dynamics.World;
 
 public class Box2DEngine {
-    World world;
-    Body groundBody;
-    Universe universe;
-    
-    /**
-     * Creates a new Box2d World and stuff.
-     *
-     * @param lowerVertex sets the lower bounds of the world in box2d coordinates (1m =
-     *                    10 px)
-     * @param upperVertex sets the upper bounds of the world in box2d coordinates (1m =
-     *                    10px)
-     * @see WorldConstants#PIX_TO_PHYSICS_RATIO
-     */
-    public Box2DEngine(final Universe universe, final float minx, final float miny, final float maxx, final float maxy) {
-        final Vec2 lowerVertex = new Vec2(minx, miny);
-        final Vec2 upperVertex = new Vec2(maxx, maxy);
-        final Vec2 gravity = new Vec2(0f, 0f);
-        final AABB aabb = new AABB(lowerVertex, upperVertex);
-        world = new World(aabb, gravity, true);
-        world.setContactListener(new RawContactHandler(universe.getEventRoot()));
-        world.setBoundaryListener(BoundaryListener.getInstance());
+	World world;
+	Body groundBody;
+	Universe universe;
 
-        // we should create some physical bounding box for all elements so that
-        // you can't escape the world.
-        // but for now it's okay without it, i guess.
-        /*
-           * PolygonDef polygonDef = new PolygonDef(); polygonDef.density = 0f;
-           * polygonDef.friction = 0.1f; polygonDef.setAsBox(100f, 10f);
-           *
-           * BodyDef groundBodyDef = new BodyDef(); groundBodyDef.position.set(0f,
-           * -10f); // has zero mass, therefore it is immovable groundBody =
-           * world.createBody(groundBodyDef); groundBody.createShape(polygonDef);
-           */
+	/**
+	 * Creates a new Box2d World and stuff.
+	 *
+	 * @param lowerVertex sets the lower bounds of the world in box2d coordinates (1m =
+	 *                    10 px)
+	 * @param upperVertex sets the upper bounds of the world in box2d coordinates (1m =
+	 *                    10px)
+	 * @see WorldConstants#PIX_TO_PHYSICS_RATIO
+	 */
+	public Box2DEngine(final Universe universe, final float minx, final float miny, final float maxx, final float maxy) {
+		final Vec2 lowerVertex = new Vec2(minx, miny);
+		final Vec2 upperVertex = new Vec2(maxx, maxy);
+		final Vec2 gravity = new Vec2(0f, 0f);
+		final AABB aabb = new AABB(lowerVertex, upperVertex);
+		world = new World(aabb, gravity, true);
+		world.setContactListener(new RawContactHandler(universe));
+		world.setBoundaryListener(BoundaryListener.getInstance());
 
-    }
+		// we should create some physical bounding box for all elements so that
+		// you can't escape the world.
+		// but for now it's okay without it, i guess.
+		/*
+		 * PolygonDef polygonDef = new PolygonDef(); polygonDef.density = 0f;
+		 * polygonDef.friction = 0.1f; polygonDef.setAsBox(100f, 10f);
+		 *
+		 * BodyDef groundBodyDef = new BodyDef(); groundBodyDef.position.set(0f,
+		 * -10f); // has zero mass, therefore it is immovable groundBody =
+		 * world.createBody(groundBodyDef); groundBody.createShape(polygonDef);
+		 */
 
-    public World getWorld() {
-        return world;
-    }
+	}
 
-    public void step(final float dt, final int iterations) {
+	public World getWorld() {
+		return world;
+	}
 
-        //calculate next physics engine step
-        world.step(dt, iterations);
-    }
+	public void step(final float dt, final int iterations) {
+
+		//calculate next physics engine step
+		world.step(dt, iterations);
+	}
 
 
 }
--- server/src/net/gtamps/game/universe/Universe.java
+++ server/src/net/gtamps/game/universe/Universe.java
@@ -17,115 +17,115 @@ import net.gtamps.shared.game.event.IGameEventListener;
 
 public class Universe implements IGameEventListener, IGameEventDispatcher {
 
-    private static final long serialVersionUID = 1821222727619509975L;
+	private static final long serialVersionUID = 1821222727619509975L;
 
-    private transient GameEventDispatcher eventRoot = new GameEventDispatcher();
+	private transient GameEventDispatcher eventRoot = new GameEventDispatcher();
 
-    private final String name;
-    private final int width;
-    private final int height;
+	private final String name;
+	private final int width;
+	private final int height;
 
-    //private LinkedList<Entity> entityList = new LinkedList<Entity>();
-    private final LinkedList<Entity> spawnPoints = new LinkedList<Entity>();
-    public Box2DEngine physics;
-    public final EventManager eventManager;
-    public final PlayerManager playerManager;
-    public final EntityManager entityManager;
+	//private LinkedList<Entity> entityList = new LinkedList<Entity>();
+	private final LinkedList<Entity> spawnPoints = new LinkedList<Entity>();
+	public Box2DEngine physics;
+	public final EventManager eventManager;
+	public final PlayerManager playerManager;
+	public final EntityManager entityManager;
 
-    private long revision;
+	private long revision;
 
-    // TODO universe builder!
+	// TODO universe builder!
 
-    public Universe(final String name, final int width, final int height) {
-        GUILogger.i().log(LogType.GAMEWORLD, "GameWorld was created, size: " + width + "x" + height);
-        this.name = name;
-        this.width = width;
-        this.height = height;
-        eventManager = new EventManager(this);
-        entityManager = new EntityManager(this);
-        playerManager = new PlayerManager(this);
+	public Universe(final String name, final int width, final int height) {
+		GUILogger.i().log(LogType.GAMEWORLD, "GameWorld was created, size: " + width + "x" + height);
+		this.name = name;
+		this.width = width;
+		this.height = height;
+		eventManager = new EventManager(this);
+		entityManager = new EntityManager(this);
+		playerManager = new PlayerManager(this);
 
-        eventManager.addEventListener(EventType.ACTION_EVENT, playerManager);
-        eventManager.addEventListener(EventType.SESSION_EVENT, entityManager);
-        eventManager.addEventListener(EventType.SESSION_EVENT, playerManager);
-        playerManager.addEventListener(EventType.PLAYER_EVENT, eventManager);
-        entityManager.addEventListener(EventType.ENTITY_EVENT, eventManager);
+		addEventListener(EventType.SESSION_EVENT, playerManager);
+		addEventListener(EventType.SESSION_EVENT, entityManager);
+		addEventListener(EventType.ACTION_EVENT, playerManager);
+		addEventListener(EventType.PLAYER_EVENT, playerManager);
+		addEventListener(EventType.ENTITY_EVENT, entityManager);
 
-    }
-    public void setPhysics (final Box2DEngine physics)  {
-    	if (physics == null) {
+	}
+	public void setPhysics (final Box2DEngine physics)  {
+		if (physics == null) {
 			throw new IllegalArgumentException("'physics' must not be 'null'");
 		}
-    	this.physics = physics;
-    }
+		this.physics = physics;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public long getRevision() {
-        return revision;
-    }
+	public long getRevision() {
+		return revision;
+	}
 
-    public void updateRevision(final long newRevision) {
-        revision = newRevision;
-    }
+	public void updateRevision(final long newRevision) {
+		revision = newRevision;
+	}
 
-    public int getWidth() {
-        return width;
-    }
+	public int getWidth() {
+		return width;
+	}
 
-    public int getHeigth() {
-        return height;
-    }
+	public int getHeigth() {
+		return height;
+	}
 
-    public Box2DEngine getPhysics() {
-        return physics;
-    }
-    
-    public IGameEventDispatcher getEventRoot() {
-    	return eventRoot;
-    }
+	public Box2DEngine getPhysics() {
+		return physics;
+	}
 
-    public void addSpawnPoint(final Entity sp) {
-        if (sp == null) {
-            throw new IllegalArgumentException("'sp' must not be null");
-        }
-        spawnPoints.add(sp);
-    }
+	public IGameEventDispatcher getEventRoot() {
+		return eventRoot;
+	}
 
-    public Entity getRandomSpawnPoint() {
-    	if (spawnPoints.size() > 0 ) {
-    		return spawnPoints.get((int) (spawnPoints.size() * Math.random()));
-    	}
-    	return null;
-    }
-    
-    @Override
-    public void addEventListener(final EventType type, final IGameEventListener listener) {
-        eventRoot.addEventListener(type, listener);
-    }
+	public void addSpawnPoint(final Entity sp) {
+		if (sp == null) {
+			throw new IllegalArgumentException("'sp' must not be null");
+		}
+		spawnPoints.add(sp);
+	}
 
-    @Override
-    public void removeEventListener(final EventType type, final IGameEventListener listener) {
-        eventRoot.removeEventListener(type, listener);
-    }
+	public Entity getRandomSpawnPoint() {
+		if (spawnPoints.size() > 0 ) {
+			return spawnPoints.get((int) (spawnPoints.size() * Math.random()));
+		}
+		return null;
+	}
 
-    @Override
-    public void dispatchEvent(final GameEvent event) {
-        eventRoot.dispatchEvent(event);
+	@Override
+	public void addEventListener(final EventType type, final IGameEventListener listener) {
+		eventRoot.addEventListener(type, listener);
+	}
 
-    }
+	@Override
+	public void removeEventListener(final EventType type, final IGameEventListener listener) {
+		eventRoot.removeEventListener(type, listener);
+	}
 
-    @Override
-    public boolean isRegisteredListener(final IGameEventListener listener) {
-    	return eventRoot.isRegisteredListener(listener);
-    }
+	@Override
+	public void dispatchEvent(final GameEvent event) {
+		eventRoot.dispatchEvent(event);
 
-    @Override
-    public void receiveEvent(final GameEvent event) {
-        dispatchEvent(event);
+	}
 
-    }
+	@Override
+	public boolean isRegisteredListener(final IGameEventListener listener) {
+		return eventRoot.isRegisteredListener(listener);
+	}
+
+	@Override
+	public void receiveEvent(final GameEvent event) {
+		dispatchEvent(event);
+
+	}
 
 }
