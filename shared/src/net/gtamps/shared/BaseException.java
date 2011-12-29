934ea97635e6fe8948341b591150fd96b8b72f28
Jan Rabe <cloudgazer3d@gmail.com>
2011-12-29 20:05:13 +0100

    type workaround

--- shared/src/net/gtamps/shared/BaseException.java
+++ shared/src/net/gtamps/shared/BaseException.java
@@ -18,9 +18,4 @@ public class BaseException extends Exception {
 	public BaseException(final String message, final Throwable cause) {
 		super(message, cause);
 	}
-
-	public BaseException(final String message, final Throwable cause, final boolean enableSuppression, final boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
 }
--- shared/src/net/gtamps/shared/serializer/communication/AbstractSendable.java
+++ shared/src/net/gtamps/shared/serializer/communication/AbstractSendable.java
@@ -10,16 +10,9 @@ public abstract class AbstractSendable<Type extends AbstractSendable<Type>> impl
 	private static final long serialVersionUID = 7512510685123238578L;
 
 
-	final Class<? extends AbstractSendable<?>> type;
 	IObjectCache<Type> cache = null;
 
-	protected AbstractSendable(final Class<? extends AbstractSendable<?>> type) {
-		assert (type == null);
-		this.type = type;
-	}
-
-	public Class<? extends AbstractSendable<?>> getType() {
-		return this.type;
+	protected AbstractSendable() {
 	}
 
 	@Override
