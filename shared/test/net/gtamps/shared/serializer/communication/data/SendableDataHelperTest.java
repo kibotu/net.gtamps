3fa344eda621325e6ef43355de2d865cdaf4b7d8
til <til.boerner@gmx.net>
2012-01-16 12:35:53 +0100

    SendableDataHelper: added a some tests

--- shared/test/net/gtamps/shared/serializer/communication/data/SendableDataHelperTest.java
+++ shared/test/net/gtamps/shared/serializer/communication/data/SendableDataHelperTest.java
@@ -1,10 +1,15 @@
 package net.gtamps.shared.serializer.communication.data;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import net.gtamps.shared.Utils.cache.TypableObjectCacheFactory;
+import net.gtamps.shared.game.GameObject;
 import net.gtamps.shared.game.entity.Entity;
 import net.gtamps.shared.serializer.communication.SendableCacheFactory;
 import net.gtamps.shared.serializer.communication.SendableProvider;
@@ -16,11 +21,9 @@ import org.junit.Test;
 public class SendableDataHelperTest {
 
 	private static final int ENTITY_UID_2 = 2;
-
 	private static final int ENTITY_UID_1 = 1;
 
 	private static final String ENTITY_NAME_2 = "name2";
-
 	private static final String ENTITY_NAME_1 = "name1";
 
 	private final TypableObjectCacheFactory cacheFactory = new SendableCacheFactory();
@@ -74,10 +77,30 @@ public class SendableDataHelperTest {
 		fail("Not yet implemented"); // TODO
 	}
 
+	@Test(expected =IllegalArgumentException.class)
+	public final void testToSendableDataTSendableProvider_whenNullElement_expectException() {
+		SendableDataHelper.toSendableData((GameObject) null, provider);
+	}
+
+	@Test(expected =IllegalArgumentException.class)
+	public final void testToSendableDataTSendableProvider_whenNullProvider_expectException() {
+		SendableDataHelper.toSendableData(someEntity, null);
+	}
+
 	@Test
 	public final void testToSendableDataTSendableProvider() {
 		fail("Not yet implemented"); // TODO
 	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public final void testToSendableDataListOfTSendableProvider_whenNullList_expectException() {
+		SendableDataHelper.toSendableData((List<GameObject>)null, provider);
+	}	
+
+	@Test(expected = IllegalArgumentException.class)
+	public final void testToSendableDataListOfTSendableProvider_whenNullProvider_expectException() {
+		SendableDataHelper.toSendableData(Collections.<GameObject>emptyList(), null);
+	}	
 
 	@Test
 	public final void testToSendableDataListOfTSendableProvider_shouldReturnListNode() {
@@ -106,7 +129,22 @@ public class SendableDataHelperTest {
 
 	@Test
 	public final void testToSendableDataListOfTSendableProvider_whenNonEmptyList_shouldReturnSameSizeListData() {
-		fail("Not yet implemented"); // TODO
+		final List<Entity> testlist = Arrays.asList(new Entity[]{someEntity, differentEntity});
+
+		final ListNode<DataMap> testee = (ListNode<DataMap>) SendableDataHelper.toSendableData(testlist, provider);
+
+		try {
+			int i = 0;
+			for (final DataMap map: testee) {
+				assertEquals(testlist.get(i).getUid(), SendableDataHelper.getGameObjectUid(map));
+				i++;
+			}
+			if (i < testlist.size()) {
+				fail("generated list is shorter than input list");
+			}
+		} catch (final IndexOutOfBoundsException e) {
+			fail("generated list is longer than input list");
+		}
 	}
 
 }
