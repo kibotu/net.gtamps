5abb57525a095b40784318570f303ba6c435437d
til <til.boerner@gmx.net>
2012-01-22 19:09:54 +0100

    added Property names to StringConstants

--- shared/src/net/gtamps/shared/serializer/communication/StringConstants.java
+++ shared/src/net/gtamps/shared/serializer/communication/StringConstants.java
@@ -1,15 +1,22 @@
 package net.gtamps.shared.serializer.communication;
 
 public class StringConstants {
-	public static final  String UPDATE_REVISION = "rev";
-	public static final  String UPDATE_REVISION_OLD = "revold";
-	public static final  String UPDATE_ENTITIES = "updateEnt";
-	public static final  String UPDATE_GAMEEVENTS = "updateEvt";
-	public static final  String PLAYER_DATA = "playerData";
-	public static final  String ERROR_MESSAGE = "errorMsg";
-	public static final  String SESSION_ID = "sessionID";
-	public static final  String AUTH_USERNAME = "authuser";
-	public static final  String AUTH_PASSWORD = "authpass";
+	public static final String UPDATE_REVISION = "rev";
+	public static final String UPDATE_REVISION_OLD = "revold";
+	public static final String UPDATE_ENTITIES = "updateEnt";
+	public static final String UPDATE_GAMEEVENTS = "updateEvt";
+	public static final String PLAYER_DATA = "playerData";
+	public static final String ERROR_MESSAGE = "errorMsg";
+	public static final String SESSION_ID = "sessionID";
+	public static final String AUTH_USERNAME = "authuser";
+	public static final String AUTH_PASSWORD = "authpass";
 
-
+	public static final String PROPERTY_POSX = "posx";
+	public static final String PROPERTY_POSY = "posy";
+	public static final String PROPERTY_ROTA = "rota";
+	public static final String PROPERTY_SPEEDX = "speedx";
+	public static final String PROPERTY_SPEEDY = "speedy";
+	public static final String PROPERTY_TYPE = "type";
+	public static final String PROPERTY_SOURCE_UID = "sourceUid";
+	public static final String PROPERTY_TARGET_UID = "targetUid";
 }
