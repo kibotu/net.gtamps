8677d0f87c8f41010adcb62cb49df6feff2d8acc
til <til.boerner@gmx.net>
2012-02-07 15:04:18 +0100

    added test cases for FilteringCollectionWrapper

--- shared/test/net/gtamps/shared/Utils/predicate/FilteringCollectionWrapperTest.java
+++ shared/test/net/gtamps/shared/Utils/predicate/FilteringCollectionWrapperTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Collection;
@@ -21,6 +22,8 @@ import org.mockito.runners.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class FilteringCollectionWrapperTest {
 
+	private static final Predicate<Object> TRUE = Predicates.alwaysTrue();
+	private static final Predicate<Object> FALSE = Predicates.alwaysFalse();
 	private static Object[] BACKING_ARRAY = {new Object(), new Object(), new Object()};
 	private static final int BACKING_COLLECTION_SIZE = BACKING_ARRAY.length;
 	@Mock
@@ -71,17 +74,17 @@ public class FilteringCollectionWrapperTest {
 
 		@Test
 		public final void testSize_whenAlwaysTrueFilter_shouldEqualBackingSize() {
-			assertEquals(BACKING_COLLECTION_SIZE, testee.withFilter(Predicates.alwaysTrue()).size());
+			assertEquals(BACKING_COLLECTION_SIZE, testee.withFilter(TRUE).size());
 		}
 
 		@Test
 		public final void testSize_whenAlwaysFalseFilter_shouldEqualZero() {
-			assertEquals(0, testee.withFilter(Predicates.alwaysFalse()).size());
+			assertEquals(0, testee.withFilter(FALSE).size());
 		}
 
 		@Test
 		public final void testSize_whenSpecificElementFilter_shouldEqualOne() {
-			assertEquals(1, testee.withFilter(singleObjectFilter()).size());
+			assertEquals(1, testee.withFilter(trueOnce()).size());
 		}
 
 		@Test
@@ -104,17 +107,17 @@ public class FilteringCollectionWrapperTest {
 
 		@Test
 		public final void testTrueForAll_whenTrueFilter_expectTrue() {
-			assertTrue(testee.withFilter(Predicates.alwaysTrue()).trueForAll());
+			assertTrue(testee.withFilter(TRUE).trueForAll());
 		}
 
 		@Test
 		public final void testTrueForAll_whenFalseFilter_expectFalse() {
-			assertFalse(testee.withFilter(Predicates.alwaysFalse()).trueForAll());
+			assertFalse(testee.withFilter(FALSE).trueForAll());
 		}
 
 		@Test
 		public final void testTrueForAll_whenSpecificElementFilter_expectFalse() {
-			assertFalse(testee.withFilter(singleObjectFilter()).trueForAll());
+			assertFalse(testee.withFilter(trueOnce()).trueForAll());
 		}
 
 		@Test
@@ -125,17 +128,17 @@ public class FilteringCollectionWrapperTest {
 
 		@Test
 		public final void testTrueForOne_whenTrueFilter_expectTrue() {
-			assertTrue(testee.withFilter(Predicates.alwaysTrue()).trueForOne());
+			assertTrue(testee.withFilter(TRUE).trueForOne());
 		}
 
 		@Test
 		public final void testTrueForOne_whenFalseFilter_expectFalse() {
-			assertFalse(testee.withFilter(Predicates.alwaysFalse()).trueForOne());
+			assertFalse(testee.withFilter(FALSE).trueForOne());
 		}
 
 		@Test
 		public final void testTrueForOne_whenSpecificElementFilter_expectTrue() {
-			assertTrue(testee.withFilter(singleObjectFilter()).trueForOne());
+			assertTrue(testee.withFilter(trueOnce()).trueForOne());
 		}
 
 		@Test(expected = IllegalArgumentException.class)
@@ -148,7 +151,21 @@ public class FilteringCollectionWrapperTest {
 			assertSame(filter, testee.withFilter(filter).getFilter());
 		}
 
-		private Predicate<Object> singleObjectFilter() {
+		@Test
+		public final void testAdd_whenFilterRejectsElement_shouldReturnFalse(){
+			assertFalse(testee.withFilter(FALSE).add(new Object()));
+		}
+
+		@Test
+		public final void testAdd_whenFilterAcceptsElement_shouldDelegateToBackingCollection(){
+			final Object newElement = new Object();
+
+			testee.withFilter(TRUE).add(newElement);
+
+			verify(backingCollection).add(newElement);
+		}
+
+		private Predicate<Object> trueOnce() {
 			when(filter.appliesTo(anyObject())).thenReturn(true, false);
 			return filter;
 		}
