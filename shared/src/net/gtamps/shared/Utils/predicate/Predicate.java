8a4026f6ca69a8b10a9ae85790e04d444078f0ac
til <til.boerner@gmx.net>
2012-02-07 01:24:49 +0100

    added doc comments to Predicate

--- shared/src/net/gtamps/shared/Utils/predicate/Predicate.java
+++ shared/src/net/gtamps/shared/Utils/predicate/Predicate.java
@@ -1,5 +1,18 @@
 package net.gtamps.shared.Utils.predicate;
 
+/**
+ * Represents a function that maps a boolean value to an argument object.
+ *
+ * @author Jan Rabe, Tom Wallroth, Til Boerner
+ *
+ * @param <T>
+ */
 public interface Predicate<T> {
-    boolean appliesTo(T x);
+	/**
+	 * {@code true} if the argument satisfies the semantics of this predicate
+	 * 
+	 * @param x	the test object to be evaluated
+	 * @return	{@true} or {@code false}
+	 */
+	boolean appliesTo(T x);
 }
